7fc6fd398672af7e7a766e75428da953c280a868
MarkusKL <markus@krabbelarsen.dk>
2021-10-29 15:59:40 +0200

    Update floyd-warshall.cpp

--- Graph/Multiple-sources-shortest-path/floyd-warshall.cpp
+++ Graph/Multiple-sources-shortest-path/floyd-warshall.cpp
@@ -4,15 +4,15 @@
 
 using namespace std;
 
-int INF = 1 << 29;
+ll INF = 1L << 61;
 
 // Takes a n*n dist vector where dist[i][j] is the distance from i to j. If no edge is connecting i to j then set dist[i][j] = INF
-void floydWarshall(vvi &dist) {
+void floydWarshall(vvll &dist) {
   int n = dist.size();
   for(int k = 0; k < n; ++k) {
     for(int i = 0; i < n; ++i) {
       for(int j = 0; j < n; ++j) {
-        dist[i][i] = min(dist[i][j], dist[i][k] + dist[k][i])
+        dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
       }
     }
   }
